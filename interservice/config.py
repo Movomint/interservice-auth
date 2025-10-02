39b74589e8b743d2436cbab1fd750d8cf8495675
William Anderson <wanderson.wa252@gmail.com>
2025-10-02 17:42:06 -0400

    added movotime to registry

--- interservice/config.py
+++ interservice/config.py
@@ -4,14 +4,14 @@ from enum import Enum
 CORE_API_BASE_URL = os.environ.get('CORE_API_BASE_URL', 'http://localhost:8000')
 DATABASE_API_BASE_URL = os.environ.get('DATABASE_API_BASE_URL', 'http://localhost:8001')
 EXTRACTION_AGENT_BASE_URL = os.environ.get('EXTRACTION_AGENT_BASE_URL', 'http://localhost:8002')
-LOAD_PLAN_PRO_BASE_URL = os.environ.get('LOAD_PLAN_PRO_BASE_URL', 'http://localhost:8003')
+MOVOTIME_BASE_URL = os.environ.get('MOVOTIME_BASE_URL', 'http://localhost:8003')
 GATEWAY_BASE_URL = os.environ.get('GATEWAY_BASE_URL', 'https://gateway.movomint.com')
 
 class Services(str, Enum):
     CORE_API = 'core-api'
     DATABASE = 'database'
     EXTRACTION_AGENT = 'extraction-agent'
-    LOAD_PLAN_PRO = 'load-plan-pro'
+    MOVOTIME = 'movotime'
     GATEWAY = 'gateway-service'
 
 
@@ -19,7 +19,7 @@ SERVICE_REGISTRY: dict[Services, str] = {
     Services.CORE_API: CORE_API_BASE_URL,
     Services.DATABASE: DATABASE_API_BASE_URL,
     Services.EXTRACTION_AGENT: EXTRACTION_AGENT_BASE_URL,
-    Services.LOAD_PLAN_PRO:  LOAD_PLAN_PRO_BASE_URL,
+    Services.MOVOTIME:  MOVOTIME_BASE_URL,
     Services.GATEWAY: GATEWAY_BASE_URL
 }
 
