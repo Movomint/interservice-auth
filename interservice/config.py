f1be3a50714cd9cbeb6f6adb4b24bd9b2d5d4596
William Anderson <wanderson.wa252@gmail.com>
2025-10-27 22:15:21 -0400

    base url changes

--- interservice/config.py
+++ interservice/config.py
@@ -3,19 +3,19 @@ from enum import Enum
 
 CORE_API_BASE_URL = os.environ.get('CORE_API_BASE_URL', 'http://core-api:8000')
 DATABASE_API_BASE_URL = os.environ.get('DATABASE_API_BASE_URL', 'http://database-api:8001')
-ORDER_ENTRY_BASE_URL = os.environ.get('ORDER_ENTRY_BASE_URL', 'http://order-entry:8002')
+AGENT_PLATFORM_BASE_URL = os.environ.get('AGENT_PLATFORM_BASE_URL', 'http://agent-platform:8002')
 INTERNAL_API_BASE_URL = os.environ.get('INTERNAL_API_BASE_URL', 'http://internal-api:8005')
 
 class Services(str, Enum):
     CORE_API = 'core-api'
-    ORDER_ENTRY = 'order-entry'
+    AGENT_PLATFORM = 'agent-platform'
     DATABASE = 'database'
     INTERNAL_API = 'internal-api'
 
 SERVICE_REGISTRY: dict[Services, str] = {
     Services.CORE_API: CORE_API_BASE_URL,
     Services.DATABASE: DATABASE_API_BASE_URL,
-    Services.ORDER_ENTRY: ORDER_ENTRY_BASE_URL,
+    Services.AGENT_PLATFORM: AGENT_PLATFORM_BASE_URL,
     Services.INTERNAL_API: INTERNAL_API_BASE_URL
 }
 
